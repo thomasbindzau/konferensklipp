904c784acdc5c50b290462bd2b322f58e99209c7
Thomas Bindzau <thomas.bindzau@gmail.com>
2015-12-11 08:41:29 +0100

    First version

---
+++ Webb/userapp/search/search.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('userApp.search', ['ngRoute'])
+        .config([
+            '$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/search', {
+                    templateUrl: 'search/search.html',
+                    controller: 'SearchController',
+                    controllerAs: 'vm'
+                });
+            }
+        ]);
+
+    angular
+    .module('userApp.start')
+    .controller('SearchController', searchController);
+
+    searchController.$inject = ['$window', '$location'];
+
+    function searchController($window, $location) {
+        var vm = this;
+
+    }
+})();
